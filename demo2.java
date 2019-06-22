18d6ad3b9bbd87499528ea87aeea0bb79f15dc55
anusha <anusha.harinarthini@gmail.com>
2019-06-22 17:25:27 +0000

    again new lines added in 2nd file

--- demo2.java
+++ demo2.java
@@ -1,3 +1,4 @@
 total 4
--rw-r--r-- 1 root root 57 Jun 22 17:15 demo1.java
--rw-r--r-- 1 root root  0 Jun 22 17:18 demo2.java
+-rw-r--r-- 1 root root 57 Jun 22 17:15 demo1.javanew lines added again
+
+--r-- 1 root root  0 Jun 22 17:18 demo2.java
